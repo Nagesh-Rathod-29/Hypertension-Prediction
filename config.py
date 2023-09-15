78c212497cdd5d0b20f5b5460eca1cfaf5b9361a
nagesh rathod <rathodnagesh0005@gmail.com>
2023-09-15 11:46:36 +0530

    Added configuration file

---
+++ config.py
@@ -0,0 +1,11 @@
+MODEL_FILE_PATH = r"artifacts//Model-1.0.h5"
+
+SCALER_FILE_PATH = r"artifacts//scaler.pkl"
+
+MODEL_FEATURES_FILE = r"artifacts//Model_data.json" 
+
+MODEL_DATA_FILE = r"data//hypertension_data.csv"
+
+PORT = 8002
+
+VERSION = '1.0.0'
